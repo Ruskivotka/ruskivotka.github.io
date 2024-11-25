5a8713696cb3ba41187b53160b998099f2a61a28
Joni <121474829+Ruskivotka@users.noreply.github.com>
2024-11-25 08:40:40 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["mechanical engineer", "design engineer", "virtual reality developer", "product designer"],
+    strings: ["mechanical engineer", "design engineer", "virtual reality developer", "product designer", "someone that your company needs"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
